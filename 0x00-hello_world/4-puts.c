fedfe18781a7b1d6bdb40c304d26c71c0039a063
Stanvirgo <stanvirgo7@gmail.com>
2022-10-14 12:00:29 -0700

    Put in

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,18 +1,12 @@
 #include <stdio.h>
 
 /**
- *
  * main - entry point
- *
- * *
  *
  * Return: 0 after the function
  */
 int main(void)
-
 {
 	puts("\"Programming is like building a multilingual puzzle");
-
 	return (0);
-
 }
